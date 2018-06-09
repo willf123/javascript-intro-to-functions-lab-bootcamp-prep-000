b4412072c5b243f0db1b7fc77a3d0190e4e3509a
willf123@yahoo.com <willf123@yahoo.com>
2018-06-09 15:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!";
-  } else if {
-    if (string.toUpperCase() === string) {
+  } else if (string.toUpperCase() === string) {
   return "YES INDEED!";
   }
   
