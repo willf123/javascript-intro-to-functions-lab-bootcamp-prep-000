233f5d18fb760aee0ca89f47dbad4e5bbb8e1fee
willf123@yahoo.com <willf123@yahoo.com>
2018-06-09 15:14:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } else if (string.toUpperCase() === string) {
   return "YES INDEED!";
-  }
-  
+  } else if ( if `string` is "I love you, Grandma.")
+  returns "I love you, too."
 }
 
